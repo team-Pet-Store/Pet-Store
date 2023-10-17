3e1d6a682cd78e1048b379460050e301439f3909
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-10-17 18:08:12 +0200

    pulled from main

--- Server/model/index.js
+++ Server/model/index.js
@@ -1,15 +1,14 @@
-const { Sequelize, DataTypes } = require('sequelize')
-require('dotenv').config()
+const { Sequelize, DataTypes } = require("sequelize");
+require("dotenv").config();
 
-
-
-
-const sequelize = new Sequelize('PetsDB', process.env.DB_USER, process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: "mysql",
-    },
-
+const sequelize = new Sequelize(
+  "petsdb",
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
 );
 
 const db = {};
@@ -17,29 +16,27 @@ const db = {};
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-
 db.User = require("./user.model")(sequelize, DataTypes);
 db.Product = require("./product.model")(sequelize, DataTypes);
 db.Cart = require("./cart.model")(sequelize, DataTypes);
 
-
-db.User.belongsToMany(db.Product, { through: db.Cart, foreignKey: 'user_id' });
-db.Product.belongsToMany(db.User, { through: db.Cart, foreignKey: 'product_id' });
+db.User.belongsToMany(db.Product, { through: db.Cart, foreignKey: "user_id" });
+db.Product.belongsToMany(db.User, {
+  through: db.Cart,
+  foreignKey: "product_id",
+});
 
 const connect = async () => {
-    try {
-        await db.sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await db.sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
-}
+// db.sequelize.sync({ force: true });
 
-
-//db.sequelize.sync({ force: true })
-
-connect()
-
+connect();
 
 module.exports = db;
