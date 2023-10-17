bd56be6878f46581b152495266b44ec66af80243
Adam <abejaoui90@gmail.com>
2023-10-17 17:40:29 +0100

    admin and user styling interface done

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { UsersListComponent } from './admin-components/users-list/users-list.component';
+import { AdminNavigationBarComponent } from './admin-components/admin-navigation-bar/admin-navigation-bar.component';
+import { ProductsListComponent } from './admin-components/products-list/products-list.component';
+
 
 @NgModule({
   declarations: [
@@ -13,10 +18,15 @@ import { CardsComponent } from './components/cards/cards.component';
     NavigationBarComponent,
     LoginComponent,
     SignupComponent,
-    CardsComponent
+    CardsComponent,
+    UsersListComponent,
+    AdminNavigationBarComponent,
+    ProductsListComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,  
+     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
