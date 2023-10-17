b081c008d689d98fc0b1d00e7926575fd60a18bb
Oumaima-chiha <chihaouma@gmail.com>
2023-10-17 15:03:16 +0100

    cart css done

---
+++ Client/src/app/components/cart/cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  template:'<div>Hello</div>',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+
+}
