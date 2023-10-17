1573aa57856576b0d33e23ee2bc74a1fceb179da
Adam <abejaoui90@gmail.com>
2023-10-17 14:50:26 +0100

    add models

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    LoginComponent,
+    SignupComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
